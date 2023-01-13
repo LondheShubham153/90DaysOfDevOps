6183fb28fb4cf6359ba30499cd43b089487602ed
abhinav2712 <abhinav.kumar2712@gmail.com>
2023-01-13 14:20:06 +0530

    solutions for day04 added

---
+++ 2023/day04/solutions/scripts/answer1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "I will complete #90DaysOofDevOps challenge"
+
+
---
+++ 2023/day04/solutions/scripts/answer2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+echo "Enter a value: "
+read user_input
+
+# Take input from command line arguments
+arg1=$1
+arg2=$2
+
+# Print the variables
+echo "The user input is: $user_input"
+echo "The first argument is: $arg1"
+echo "The second argument is: $arg2"
