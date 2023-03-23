a3a9eedd7321bf1e66bf2b55e004f116b4f06be3
Sumit Ramchandra Ghorpade <119837985+SumitRamchandra@users.noreply.github.com>
2023-03-23 10:35:54 +0530

    script code added

---
+++ 2023/day04/Submission/input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+    #get user's name as input
+       echo "what is your name:"
+	    read name
+
+	        #input from arguments
+		    position=$1
+
+		        #print the variable
+			    echo "name:$name"
+			        echo "position:$position"
---
+++ 2023/day04/Submission/num.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+num1=$1
+num2=$2
+
+if [ $num1 -gt $num2 ]
+then
+  echo "$num1 is greater than $num2"
+  else
+    echo "$num2 is greater than $num1"
+    fi
+
