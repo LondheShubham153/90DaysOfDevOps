baf4459dadaf16f5475888530fdc0fc0fffbe2c5
SumitRamchandra <sumitg4797@gmail.com>
2023-04-02 11:28:02 +0530

    My_Submission

--- 2023/day14/solution/python.py
+++ 2023/day14/solution/python.py
@@ -1,31 +1,34 @@
+#Task 1.
+
 #list - A list is a collection of ordered elements.
 
-#mobile_co = ["Samsung", "Nokia", "Apple", "Oneplus", "Motorola"]
-#print(type(mobile_co))
-#print(mobile_co)
+mobile_co = ["Samsung", "Nokia", "Apple", "Oneplus", "Motorola"]
+print(type(mobile_co))
+print(mobile_co)
 
 #Tuple - A tuple is a collection of ordered elements.
 
-#my_tuple = (1, 2, 3, 'four', 'five')
-#print(type(my_tuple))
-#print(my_tuple)
+my_tuple = (1, 2, 3, 'four', 'five')
+print(type(my_tuple))
+print(my_tuple)
 
 #Set - A set is an unordered collection of unique elements.
 
-#my_set = {1, 2, 3, 4, 5}
-#print(type(my_set))
-#print(my_set)
+my_set = {1, 2, 3, 4, 5}
+print(type(my_set))
+print(my_set)
 
+#Task 2.
 # Dictionary
-#fav_tools = { 
-#  1:"Linux", 2:"Git", 3:"Docker", 4:"Kubernetes", 5:"Terraform", 6:"Ansible", 7:"Chef"
-#}
-#print(fav_tools[3])
+fav_tools = { 
+  1:"Linux", 2:"Git", 3:"Docker", 4:"Kubernetes", 5:"Terraform", 6:"Ansible", 7:"Chef"
+}
+print(fav_tools[3])
 
 
 
 
-
+#Task 3.
 cloud_providers = ["AWS", "GCP", "Azure"]
 
 # Add "Digital Ocean" to the list
