8fb3ff774d67f57bb8889bee25a1868ea7e094fb
SumitRamchandra <sumitg4797@gmail.com>
2023-04-02 11:25:06 +0530

    My_Submission

---
+++ 2023/day14/solution/python.py
@@ -0,0 +1,38 @@
+#list - A list is a collection of ordered elements.
+
+#mobile_co = ["Samsung", "Nokia", "Apple", "Oneplus", "Motorola"]
+#print(type(mobile_co))
+#print(mobile_co)
+
+#Tuple - A tuple is a collection of ordered elements.
+
+#my_tuple = (1, 2, 3, 'four', 'five')
+#print(type(my_tuple))
+#print(my_tuple)
+
+#Set - A set is an unordered collection of unique elements.
+
+#my_set = {1, 2, 3, 4, 5}
+#print(type(my_set))
+#print(my_set)
+
+# Dictionary
+#fav_tools = { 
+#  1:"Linux", 2:"Git", 3:"Docker", 4:"Kubernetes", 5:"Terraform", 6:"Ansible", 7:"Chef"
+#}
+#print(fav_tools[3])
+
+
+
+
+
+cloud_providers = ["AWS", "GCP", "Azure"]
+
+# Add "Digital Ocean" to the list
+cloud_providers.append("Digital Ocean")
+
+# Sort the list in alphabetical order
+cloud_providers.sort()
+
+# Print the sorted list
+print(cloud_providers)
