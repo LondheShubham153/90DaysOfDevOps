33bb7f65305a83f8cc8ccc6aee33f313db32221d
Supriya <surkarsupriya97@gmail.com>
2023-05-07 11:57:02 -0700

    added my_submission file and scripts and snapshot

---
+++ 2023/day04/submission/comparingnumbers.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash
+
+ #Define two numbers
+ 
+ num1=10
+ num2=20
+
+
+  #compare the numbers using an if-else statement
+  
+ if [ $num1 -gt $num2 ]
+ then
+	 echo "$num1 is greater that $num2"
+ else 
+	 echo "$num1 is less than or equal to $num2"
+
+ fi
+
