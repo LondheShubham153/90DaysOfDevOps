40d7acdd54e4bc692001ec1d835f321c1a8a2762
Supriya <surkarsupriya97@gmail.com>
2023-05-12 03:59:24 -0700

    added created 2 users snap and modified day05 my_submssion.txt

--- 2023/day05/submission/scripts/adding_user.sh
+++ 2023/day05/submission/scripts/adding_user.sh
@@ -2,14 +2,25 @@
 
 add_user()
 {
-USER=$1
-PASS=$2
+    USER=$1
+    PASS=$2
 
-useradd -m -p $PASS $USER && echo "Successfully added user"
-
+    useradd -m $USER
+    echo "$USER:$PASS" | chpasswd
+    echo "Successfully added user: $USER"
 }
 
 #MAIN
 
-add_user  User1  #test@123
+echo "Enter username for User 1:"
+read USER1
+echo "Enter password for User 1:"
+read -s PASS1
+echo "Enter username for User 2:"
+read USER2
+echo "Enter password for User 2:"
+read -s PASS2
+
+add_user $USER1 $PASS1
+add_user $USER2 $PASS2
 
