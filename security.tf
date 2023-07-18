2772fdfced81f1d9e48d9b3da4caf5a3736c92ac
AbhipreyaGhosh <86410938+AbhipreyaGhosh@users.noreply.github.com>
2023-07-19 00:14:27 +0530

    Update security.tf

---
+++ security.tf
@@ -0,0 +1,28 @@
+# alb_security_group.tf
+
+resource "aws_security_group" "alb_security_group" {
+  name_prefix = "ALBSecurityGroup-"
+  description = "Security group for the ALB"
+
+  # Define your security group rules here
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
